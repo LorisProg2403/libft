8190c911445a189fb547721cede4ec44a8cd214a
loris <loris.gaume@42.ch>
2023-10-27 01:32:35 +0200

    adding ft_unsigned itoa

--- ft_unsigned_itoa.c
+++ ft_unsigned_itoa.c
@@ -12,4 +12,33 @@
 
 #include "libft.h"
 
+static int	ft_num_len(unsigned	int num)
+{
+	int	len;
 
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 10;
+	}
+	return (len);
+}
+
+char	*ft_unsigned_itoa(unsigned int n)
+{
+	char	*num;
+	int 	len;
+
+	len = ft_num_len(n);
+	num = (char *)malloc(sizeof(char) * (len + 1));
+	if (!num)
+		return (0);
+	while (n)
+	{
+		num[len - 1] = n % 10 + '0';
+		n = n / 10;
+		len--;
+	}
+	return (num);
+}
--- libft.h
+++ libft.h
@@ -45,7 +45,6 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 char		*ft_strnstr(const char *s1, const char *s2, size_t n);
-int			ft_isspace(int c);
 int			ft_atoi(const char *str);
 char		*ft_strdup(const char *s1);
 void		*ft_calloc(size_t count, size_t size);
@@ -53,7 +52,6 @@ char		*ft_strjoin(const char *s1, const char *s2);
 char		**ft_split(const char *s, char c);
 char		*ft_strtrim(const char *s1, const char *set);
 char		*ft_itoa(int n);
-char		*ft_strnew(size_t size);
 char		*ft_substr(const char *s, unsigned int start, size_t len);
 char		*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
@@ -72,4 +70,9 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+//addings
+char		*ft_unsigned_itoa(unsigned int n);
+int			ft_isspace(int c);
+char		*ft_strnew(size_t size);
 #endif
