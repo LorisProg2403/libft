88665c9e08e87ed7233b3951d2bb99569894c7d6
lgaume <lgaume@student.42.fr>
2023-11-22 14:53:14 +0100

    add a space

--- src/ft_utils/handle_errors.c
+++ src/ft_utils/handle_errors.c
@@ -6,7 +6,7 @@
 /*   By: lgaume <lgaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:08:12 by lgaume            #+#    #+#             */
-/*   Updated: 2023/11/22 10:11:51 by lgaume           ###   ########.fr       */
+/*   Updated: 2023/11/22 14:53:05 by lgaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 
 void	handle_errors(char *error_msg)
 {
-	ft_printf("%e%e%e", "Error :", error_msg, "\n");
+	ft_printf("%e%e%e", "Error : ", error_msg, "\n");
 	exit(1);
 }
